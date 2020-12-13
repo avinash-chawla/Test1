a759f1b0845ba52d6d4ab8945a8c9cf04a1ab8d6
Avinash <avinashchawla96@gmail.com>
2020-12-14 00:26:53 +0530

    Add project files.

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public int Test()
+        {
+            return 1;
+        }
+
+        public int Test2()
+        {
+            
+            var context = SessionSettings.sessionContext;
+            context.UserID = 3;
+
+            //SessionSettings.session.Set("SessionContext", context);
+            SessionSettings.sessionContext = context;
+
+            return (SessionSettings.sessionContext.UserID);
+        }
+
+        public int Test3()
+        {
+            SessionSettings.sessionContext.UserID = 3;
+            return (SessionSettings.sessionContext.UserID);
+        }
+    }
+}
---
+++ WebApplication6/Models/SessionSettings.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Models
+{
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonSerializer.Deserialize<T>(value);
+        }
+
+        public static void Remove(this ISession session, string key)
+        {
+            session.Remove(key);
+        }
+    }
+
+    public static class SessionSettings
+    {
+        public static ISession session = AppHttpContext.Current.Session;
+        private static SessionContext _sessionContext;
+        public static SessionContext sessionContext
+        {
+            get
+            {
+                SessionContext context = null;
+                if (session.Get<SessionContext>("SessionContext") == default(SessionContext))
+                {
+                    context = new SessionContext();
+                    session.Set<SessionContext>("SessionContext", context);
+                    sessionContext = context;
+                }
+                else
+                {
+                    context = session.Get<SessionContext>("SessionContext");
+                }
+                return context; 
+            }
+            set
+            {
+                session.Remove("SessionContext");
+                session.Set<SessionContext>("SessionContext", value);
+            }
+        }
+    }
+
+    public class SessionContext
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public static class AppHttpContext
+    {
+        static IServiceProvider services = null;
+
+        public static IServiceProvider Services
+        {
+            get { return services; }
+            set
+            {
+                if (services != null)
+                {
+                    throw new Exception("Can't set once a value has already been set.");
+                }
+                services = value;
+            }
+        }
+
+        public static HttpContext Current
+        {
+            get
+            {
+                IHttpContextAccessor httpContextAccessor = services.GetService(typeof(IHttpContextAccessor)) as IHttpContextAccessor;
+                return httpContextAccessor?.HttpContext;
+            }
+        }
+
+    }
+}
